09213a74799375f9d33a184fa3c8efbd8146624f
Paul Ramon <peramon98.18@gmail.com>
2025-08-21 11:50:57 -0500

    AppBar and actions

--- hello_world_app/lib/presentation/screens/counter/counter_screen.dart
+++ hello_world_app/lib/presentation/screens/counter/counter_screen.dart
@@ -17,27 +17,75 @@ class _CounterScreenState extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Counter Screen')),
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+        // leading:
+        actions: [
+          IconButton(
+            onPressed: () {
+              setState(() {
+                clickCounter = 0;
+              });
+            },
+            icon: const Icon(Icons.refresh_rounded),
+          ),
+        ],
+      ),
+
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center, //
           children: [
             Text(
               '$clickCounter',
-              style: const TextStyle(fontSize: 160, fontWeight: FontWeight.w100),
+              style: const TextStyle(
+                fontSize: 160,
+                fontWeight: FontWeight.w100,
+              ),
             ),
-            Text( clickCounter != 1  ? 'Clicks':'Click', style: TextStyle(fontSize: 25)),
+            Text(
+              clickCounter != 1 ? 'Clicks' : 'Click',
+              style: const TextStyle(fontSize: 25),
+            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            // setState notifica a Flutter que el estado ha cambiado y necesita reconstruir el widget
-            clickCounter++; // Incrementa el contador de clics
-          });
-        },
-        child: Icon(Icons.plus_one),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            shape: StadiumBorder(),
+            onPressed: () {
+              setState(() {
+                // setState notifica a Flutter que el estado ha cambiado y necesita reconstruir el widget
+                clickCounter++; // Incrementa el contador de clics
+              });
+            },
+            child: Icon(Icons.refresh_outlined),
+          ),
+          const SizedBox(height: 10), // Espacio entre los botones
+          FloatingActionButton(
+            shape: StadiumBorder(),
+            onPressed: () {
+              setState(() {
+                // setState notifica a Flutter que el estado ha cambiado y necesita reconstruir el widget
+                clickCounter++; // Incrementa el contador de clics
+              });
+            },
+            child: Icon(Icons.plus_one),
+          ),
+          const SizedBox(height: 10), // Espacio entre los botones
+          FloatingActionButton(
+            shape: StadiumBorder(),
+            onPressed: () {
+              setState(() {
+                // setState notifica a Flutter que el estado ha cambiado y necesita reconstruir el widget
+                clickCounter--; // Incrementa el contador de clics
+              });
+            },
+            child: Icon(Icons.exposure_minus_1),
+          ),
+        ],
       ),
     );
   }
