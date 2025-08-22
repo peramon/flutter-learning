d187dd3df13e9030dc7fb8fa7a74c97bf16e430e
Paul Ramon <peramon98.18@gmail.com>
2025-08-22 08:34:22 -0500

    List area message

--- yes_no_app/lib/presentation/chat/chat_screen.dart
+++ yes_no_app/lib/presentation/chat/chat_screen.dart
@@ -18,6 +18,31 @@ class ChatScreen extends StatelessWidget {
         title: Text('Pirat'),
         centerTitle: true,
       ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) {
+                  return Text('Indice $index');
+                },
+              ),
+            ),
+            Text('Hi'),
+          ],
+        ),
+      ),
     );
   }
 }
