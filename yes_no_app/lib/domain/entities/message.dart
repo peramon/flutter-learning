f484c5e88f100eb97aa9d7ee6cbd9427850a626b
Paul Ramon <peramon98.18@gmail.com>
2025-08-29 12:05:04 -0500

    Moving Scroll

--- yes_no_app/lib/domain/entities/message.dart
+++ yes_no_app/lib/domain/entities/message.dart
@@ -5,5 +5,17 @@ class Message {
   final String? imageUrl;
   final FromWho fromWho;
 
-  Message({required this.text, this.imageUrl, required this.fromWho});
+  Message._({required this.text, this.imageUrl, required this.fromWho});
+
+  // Validar si el mensaje me llega nulo
+  factory Message({
+    required String text,
+    String? imageUrl,
+    required FromWho fromWho,
+  }) {
+    if (text.trim().isEmpty && (imageUrl == null || imageUrl.isEmpty)) {
+      throw ArgumentError('Message must contain text or an image');
+    }
+    return Message._(text: text, imageUrl: imageUrl, fromWho: fromWho);
+  }
 }
--- yes_no_app/lib/presentation/providers/chat_provider.dart
+++ yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -2,14 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
+  final ScrollController chatScrollController = ScrollController();
+
   List<Message> messageList = [
     Message(text: 'Hi candy', fromWho: FromWho.mine),
     Message(text: 'how are you ?', fromWho: FromWho.mine),
   ];
 
   Future<void> sendMessage(String text) async {
+    if (text.trim().isEmpty) return;
     final newMessage = Message(text: text, fromWho: FromWho.mine);
     messageList.add(newMessage);
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  void moveScrollToBottom() async {
+    await Future.delayed(Duration(milliseconds: 600));
+
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeInBack,
+    );
   }
 }
