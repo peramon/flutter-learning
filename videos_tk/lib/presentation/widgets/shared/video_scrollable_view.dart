0e004af36a21f37d79c62966e792929480dd3d35
Paul Ramon <peramon98.18@gmail.com>
2025-09-04 10:51:17 -0500

    Circular progress and page view

---
+++ videos_tk/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:videos_tk/domain/entities/video_post.dart';
+
+class VideoScrollableView extends StatelessWidget {
+  final List<VideoPost> videos;
+
+  const VideoScrollableView({super.key, required this.videos});
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      scrollDirection: Axis.vertical,
+      children: [
+        Container(color: Colors.red),
+        Container(color: Colors.blue),
+        Container(color: Colors.teal),
+      ],
+    );
+  }
+}
