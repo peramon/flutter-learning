077bfca1353426f5540ec346ad9e2080a06a9064
Paul Ramon <peramon98.18@gmail.com>
2025-09-18 12:06:02 -0500

    Create options aplication

--- widgets_app/lib/config/theme/app_theme.dart
+++ widgets_app/lib/config/theme/app_theme.dart
@@ -14,13 +14,14 @@ class AppTheme {
     : assert(selectedColor >= 0, 'Selected color must be greater then 0'),
       assert(
         selectedColor < colorList.length,
-        'Selected color must be less or equal then ${colorList.length-1}',
+        'Selected color must be less or equal then ${colorList.length - 1}',
       );
 
   ThemeData getTheme() {
     return ThemeData(
       useMaterial3: true,
       colorSchemeSeed: colorList[selectedColor],
+      appBarTheme: AppBarTheme(centerTitle: true),
     );
   }
 }
--- widgets_app/lib/main.dart
+++ widgets_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -12,8 +13,8 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: AppTheme(selectedColor: 3).getTheme(),
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+      theme: AppTheme(selectedColor: 2).getTheme(),
+      home: const HomeScreen(),
     );
   }
 }
